ece3d4fd2502c6f0926aefdf2b7495ca57ee69c8
Jason Lokerson <cmercenary@gmail.com>
2016-04-07 21:43:01 -0700

    Need to put autowiring tests into an enclosure

--- src/gtest-all-guard.cpp
+++ src/gtest-all-guard.cpp
@@ -1,11 +1,14 @@
 // Copyright (C) 2012-2015 Leap Motion, Inc. All rights reserved.
+#define NOMINMAX
 #include <gtest/gtest-all.cc>
+#include <autowiring/AutowiringEnclosure.h>
 
 using namespace std;
 
 int main(int argc, const char* argv[])
 {
   auto& listeners = testing::UnitTest::GetInstance()->listeners();
+  listeners.Append(new AutowiringEnclosure);
   testing::InitGoogleTest(&argc, (char**)argv);
   return RUN_ALL_TESTS();
 }
