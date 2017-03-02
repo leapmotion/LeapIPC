53357ea1fa92615072227d838bcafc30cbb8b058
Andrew Exo <aexo@leapmotion.com>
2017-03-02 10:54:22 -0800

    Add file endpoint

---
+++ src/leapipc/IPCFileEndpoint.cpp
@@ -0,0 +1,50 @@
+// Copyright (C) 2012-2016 Leap Motion, Inc. All rights reserved.
+#include "stdafx.h"
+#include "IPCFileEndpoint.h"
+#include <system_error>
+
+using namespace leap::ipc;
+
+IPCFileEndpoint::IPCFileEndpoint(const std::string & fileName, bool read, bool write)
+{
+  int mode = std::ios::binary;
+  mode |= read ? std::ios::in : 0;
+  mode |= write ? std::ios::out : 0;
+  m_file.open(fileName, mode);
+  if (!m_file.is_open())
+    throw std::runtime_error("Failed to open file");
+}
+
+IPCFileEndpoint::~IPCFileEndpoint(void)
+{
+  if (m_file.is_open())
+    m_file.close();
+}
+
+std::streamsize IPCFileEndpoint::ReadRaw(void* buffer, std::streamsize size) {
+  if (m_file.eof())
+    return Done(Reason::UserClosed);
+
+  m_file.read((char*)buffer, size);
+  return m_file.gcount();
+}
+
+bool IPCFileEndpoint::WriteRaw(const void* pBuf, std::streamsize nBytes) {
+  m_file.write((char*)pBuf, nBytes);
+  if (m_file.bad())
+    return false;
+  return true;
+}
+
+bool IPCFileEndpoint::Abort(Reason reason) {
+  if (m_file.is_open()) {
+    m_file.close();
+    return true;
+  }
+  return false;
+}
+
+// On the first call, return 0 (EOF). Subsequent calls, return -1 (error).
+int IPCFileEndpoint::Done(Reason reason) {
+  return Abort(reason) ? 0 : -1;
+}
---
+++ src/leapipc/testing/IPCFileEndpointTest.cpp
@@ -0,0 +1,43 @@
+// Copyright (C) 2012-2016 Leap Motion, Inc. All rights reserved.
+#include "stdafx.h"
+#include "IPCTestUtils.h"
+#include <leapipc/IPCFileEndpoint.h>
+#include <autowiring/autowiring.h>
+#include <autowiring/CoreThread.h>
+#include <cstdio>
+#include <gtest/gtest.h>
+
+using namespace leap::ipc;
+
+class IPCFileEndpointTest :
+  public testing::Test
+{};
+
+TEST_F(IPCFileEndpointTest, RawWriteToReadFromFile)
+{
+  std::string testMessage = "0123456789abcdef";
+  const char* testFile = "IPCFileEndpoint_TestFile";
+
+  // Write test message to file
+  {
+    std::shared_ptr<IPCFileEndpoint> ep = std::make_shared<IPCFileEndpoint>(testFile, false, true);
+    ep->WriteRaw((void*)testMessage.c_str(), testMessage.size());
+  }
+
+  // Read test message from file
+  {
+    std::shared_ptr<IPCFileEndpoint> ep = std::make_shared<IPCFileEndpoint>(testFile, true, false);
+
+    size_t bufsize = testMessage.size()+1;
+    char* buf = new char[bufsize];
+    buf[bufsize - 1] = 0;
+
+    ep->ReadRaw((void*)buf, bufsize-1);
+
+    ASSERT_EQ(strcmp(buf, testMessage.c_str()), 0);
+
+    delete[] buf;
+  }
+
+  remove(testFile);
+}
