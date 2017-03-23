ce6b4f262b1308f35aa3e20af2199b58a7d9b793
Andrew Exo <aexo@leapmotion.com>
2017-03-23 14:53:33 -0700

    Add 2 channel test for File Endpoints

--- src/leapipc/testing/IPCFileEndpointTest.cpp
+++ src/leapipc/testing/IPCFileEndpointTest.cpp
@@ -41,3 +41,59 @@ TEST_F(IPCFileEndpointTest, RawWriteToReadFromFile)
 
   remove(testFile);
 }
+
+TEST_F(IPCFileEndpointTest, FileEndpointTwoChannels)
+{
+  std::string testMessage1 = "0123456789abcdef";
+  std::string testMessage2 = "fedcba9876543210";
+  const char* testFile = "IPCFileEndpoint_TestFile2";
+
+  // Write test messages to file
+  {
+    std::shared_ptr<IPCFileEndpoint> ep = std::make_shared<IPCFileEndpoint>(testFile, false, true);
+    auto channel0 = ep->AcquireChannel(0, IPCEndpoint::Channel::WRITE_ONLY);
+    auto channel1 = ep->AcquireChannel(1, IPCEndpoint::Channel::WRITE_ONLY);
+
+    channel0->Write(testMessage1.c_str(), testMessage1.size());
+    channel0->WriteMessageComplete();
+    channel1->Write(testMessage2.c_str(), testMessage2.size());
+    channel1->WriteMessageComplete();
+  }
+
+  // Read test messages from file
+  {
+    std::shared_ptr<IPCFileEndpoint> ep = std::make_shared<IPCFileEndpoint>(testFile, true, false);
+
+    size_t bufsize = testMessage1.size() + 1;
+    char* buf = new char[bufsize];
+    buf[bufsize - 1] = 0;
+
+    // read first message
+    IPCEndpoint::Header hdr = ep->ReadMessageHeader();
+    ASSERT_EQ(hdr.channel, 0);
+    ASSERT_EQ(hdr.PayloadSize(), testMessage1.size());
+    ep->ReadPayload(buf, hdr.PayloadSize());
+    ASSERT_EQ(strcmp(buf, testMessage1.c_str()), 0);
+    delete[] buf;
+
+    hdr = ep->ReadMessageHeader(); // read empty EOM header
+    ASSERT_EQ(hdr.eom, 1);
+
+    bufsize = testMessage2.size() + 1;
+    buf = new char[bufsize];
+    buf[bufsize - 1] = 0;
+
+    // read second message
+    hdr = ep->ReadMessageHeader();
+    ASSERT_EQ(hdr.channel, 1);
+    ASSERT_EQ(hdr.PayloadSize(), testMessage2.size());
+    ep->ReadPayload(buf, hdr.PayloadSize());
+    ASSERT_EQ(strcmp(buf, testMessage2.c_str()), 0);
+    delete[] buf;
+
+    hdr = ep->ReadMessageHeader(); // read empty EOM header
+    ASSERT_EQ(hdr.eom, 1);
+  }
+
+  remove(testFile);
+}
