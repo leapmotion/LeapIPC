8335358579367af17deb59a668631fd608020b6e
Jonathan Marsden <jmarsden@leapmotion.com>
2017-03-28 14:19:22 -0700

    Reduce dependency on shared_ptr in test

--- src/leapipc/testing/CircularBufferEndpointTest.cpp
+++ src/leapipc/testing/CircularBufferEndpointTest.cpp
@@ -16,88 +16,88 @@ class CircularBufferEndpointTest :
 
 TEST_F(CircularBufferEndpointTest, WriteReadSequence)
 {
-  auto cbuf = std::make_shared<CircularBufferEndpoint>(32);
+  CircularBufferEndpoint cbuf(32);
 
   // read from buffer in separate thread
-  std::thread reader([cbuf] {
+  std::thread reader([&cbuf] {
     char t[16];
-    cbuf->ReadRaw(t, 4);
+    cbuf.ReadRaw(t, 4);
     ASSERT_EQ(strncmp(t, "abcd", 4), 0);
-    cbuf->ReadRaw(t, 8);
+    cbuf.ReadRaw(t, 8);
     ASSERT_EQ(strncmp(t, "efghijkl", 8), 0);
-    cbuf->ReadRaw(t, 16);
+    cbuf.ReadRaw(t, 16);
     ASSERT_EQ(strncmp(t, "mnopqr0123456789", 16), 0);
-    cbuf->ReadRaw(t, 6);
+    cbuf.ReadRaw(t, 6);
     ASSERT_EQ(strncmp(t, "987654", 6), 0);
-    cbuf->ReadRaw(t, 4);
+    cbuf.ReadRaw(t, 4);
     ASSERT_EQ(strncmp(t, "3210", 4), 0);
 
-    cbuf->ReadRaw(t, 4);
+    cbuf.ReadRaw(t, 4);
     ASSERT_EQ(strncmp(t, "abcd", 4), 0);
-    cbuf->ReadRaw(t, 8);
+    cbuf.ReadRaw(t, 8);
     ASSERT_EQ(strncmp(t, "efghijkl", 8), 0);
-    cbuf->ReadRaw(t, 16);
+    cbuf.ReadRaw(t, 16);
     ASSERT_EQ(strncmp(t, "mnopqr0123456789", 16), 0);
-    cbuf->ReadRaw(t, 6);
+    cbuf.ReadRaw(t, 6);
     ASSERT_EQ(strncmp(t, "987654", 6), 0);
-    cbuf->ReadRaw(t, 4);
+    cbuf.ReadRaw(t, 4);
     ASSERT_EQ(strncmp(t, "3210", 4), 0);
   });
 
   // write to buffer
-  cbuf->WriteRaw("abcdef", 6);
-  cbuf->WriteRaw("ghijkl", 6);
-  cbuf->WriteRaw("mnopqr", 6);
-  cbuf->WriteRaw("01234567899876543210", 20);
+  cbuf.WriteRaw("abcdef", 6);
+  cbuf.WriteRaw("ghijkl", 6);
+  cbuf.WriteRaw("mnopqr", 6);
+  cbuf.WriteRaw("01234567899876543210", 20);
 
-  cbuf->WriteRaw("abcdef", 6);
-  cbuf->WriteRaw("ghijkl", 6);
-  cbuf->WriteRaw("mnopqr", 6);
-  cbuf->WriteRaw("01234567899876543210", 20);
+  cbuf.WriteRaw("abcdef", 6);
+  cbuf.WriteRaw("ghijkl", 6);
+  cbuf.WriteRaw("mnopqr", 6);
+  cbuf.WriteRaw("01234567899876543210", 20);
 
   reader.join();
 }
 
 TEST_F(CircularBufferEndpointTest, IncreaseSize)
 {
-  auto cbuf = std::make_shared<CircularBufferEndpoint>(16);
+  CircularBufferEndpoint cbuf(16);
 
   // read from buffer in separate thread
-  std::thread reader([cbuf] {
+  std::thread reader([&cbuf] {
     char t[16];
-    cbuf->ReadRaw(t, 4);
+    cbuf.ReadRaw(t, 4);
     ASSERT_EQ(strncmp(t, "abcd", 4), 0);
-    cbuf->ReadRaw(t, 8);
+    cbuf.ReadRaw(t, 8);
     ASSERT_EQ(strncmp(t, "efghijkl", 8), 0);
-    cbuf->ReadRaw(t, 16);
+    cbuf.ReadRaw(t, 16);
     ASSERT_EQ(strncmp(t, "mnopqr0123456789", 16), 0);
-    cbuf->ReadRaw(t, 6);
+    cbuf.ReadRaw(t, 6);
     ASSERT_EQ(strncmp(t, "987654", 6), 0);
-    cbuf->ReadRaw(t, 4);
+    cbuf.ReadRaw(t, 4);
     ASSERT_EQ(strncmp(t, "3210", 4), 0);
 
-    cbuf->ReadRaw(t, 4);
+    cbuf.ReadRaw(t, 4);
     ASSERT_EQ(strncmp(t, "abcd", 4), 0);
-    cbuf->ReadRaw(t, 8);
+    cbuf.ReadRaw(t, 8);
     ASSERT_EQ(strncmp(t, "efghijkl", 8), 0);
-    cbuf->ReadRaw(t, 16);
+    cbuf.ReadRaw(t, 16);
     ASSERT_EQ(strncmp(t, "mnopqr0123456789", 16), 0);
-    cbuf->ReadRaw(t, 6);
+    cbuf.ReadRaw(t, 6);
     ASSERT_EQ(strncmp(t, "987654", 6), 0);
-    cbuf->ReadRaw(t, 4);
+    cbuf.ReadRaw(t, 4);
     ASSERT_EQ(strncmp(t, "3210", 4), 0);
   });
 
   // write to buffer
-  cbuf->WriteRaw("abcdef", 6);
-  cbuf->WriteRaw("ghijkl", 6);
-  cbuf->WriteRaw("mnopqr", 6);
-  cbuf->WriteRaw("01234567899876543210", 20);
+  cbuf.WriteRaw("abcdef", 6);
+  cbuf.WriteRaw("ghijkl", 6);
+  cbuf.WriteRaw("mnopqr", 6);
+  cbuf.WriteRaw("01234567899876543210", 20);
 
-  cbuf->WriteRaw("abcdef", 6);
-  cbuf->WriteRaw("ghijkl", 6);
-  cbuf->WriteRaw("mnopqr", 6);
-  cbuf->WriteRaw("01234567899876543210", 20);
+  cbuf.WriteRaw("abcdef", 6);
+  cbuf.WriteRaw("ghijkl", 6);
+  cbuf.WriteRaw("mnopqr", 6);
+  cbuf.WriteRaw("01234567899876543210", 20);
 
   reader.join();
 }
